77d3f56ed4c50b0318dc05f1264d633e66a34474
unir00t <unir00t@OPTIPLEX>
2014-01-24 23:15:11 +0900

    new

---
+++ idea4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Idea4::Application.config.secret_key_base = '40ff134e38d25c477972c316eb5e1666bca322ccd769df59aaa7b05de4693869cc8e3a4408d66aaee4f2a33306574202565bb0a005a4aff08a77eae9f9c71c1c'
